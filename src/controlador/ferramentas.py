e0fd0599d34e40bd1fb73a15ac93ebdd4094d08a
Jojoggmlp0906 <joadson.mauricio@dcomp.ufs.br>
2026-07-13 13:50:51 -0300

    Fix import path for figuras module

--- src/controlador/ferramentas.py
+++ src/controlador/ferramentas.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from modelo.figuras import Linha,Rabisco,Retangulo,Circulo,Oval
+from model.figuras import Linha,Rabisco,Retangulo,Circulo,Oval
 
 class EstadoDesenho(ABC):
     @abstractmethod
