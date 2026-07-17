572d1cf2b3985d0a6a7c6d140bbfc6de5ed2f612
Jojoggmlp0906 <joadson.mauricio@dcomp.ufs.br>
2026-07-17 16:00:59 -0300

    Add polygon and regular polygon drawing states

--- projeto_completo/estados.py
+++ projeto_completo/estados.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+from tkinter import simpledialog
+from model.figuras import Linha, Rabisco, Retangulo, Circulo, Oval, Poligono, PoligonoRegular
 from model.figuras import Linha, Rabisco, Retangulo, Circulo, Oval
 
 class EstadoDesenho(ABC):
@@ -38,7 +40,7 @@ class EstadoRabisco(EstadoDesenho):
 
     def soltar(self, event, figura_atual):
         pass
-        
+
 
 class EstadoRetangulo(EstadoDesenho):
     def pressionar(self, event, cor_borda, cor_preenchimento):
@@ -79,6 +81,53 @@ class EstadoOval(EstadoDesenho):
         pass
 
 
+class EstadoPoligono(EstadoDesenho):
+    def __init__(self):
+        self.poligono = None
+        self.pontos = []
+
+    def pressionar(self, event, cor_borda, cor_preenchimento):
+        if self.poligono is None:
+            self.pontos = [(event.x, event.y), (event.x, event.y)]
+            self.poligono = Poligono(self.pontos, cor_borda, cor_preenchimento)
+            return self.poligono
+
+        self.pontos[-1] = (event.x, event.y)
+        self.pontos.append((event.x, event.y))
+        return self.poligono
+
+    def arrastar(self, event, figura_atual):
+        if figura_atual and isinstance(figura_atual, Poligono):
+            figura_atual.pontos[-1] = (event.x, event.y)
+
+    def soltar(self, event, figura_atual):
+        pass
+
+    def finalizar(self):
+        if self.poligono and len(self.poligono.pontos) > 3:
+            self.poligono.pontos.pop()
+            self.poligono = None
+            self.pontos = []
+            return True
+        return False
+
+
+class EstadoPoligonoRegular(EstadoDesenho):
+    def pressionar(self, event, cor_borda, cor_preenchimento):
+        lados = simpledialog.askinteger("Polígono regular", "Número de lados:", parent=event.widget.master, minvalue=3, initialvalue=5)
+        if lados is None:
+            lados = []
+        return PoligonoRegular(event.x, event.y, event.x, event.y, lados, cor_borda, cor_preenchimento)
+
+    def arrastar(self, event, figura_atual):
+        if figura_atual and isinstance(figura_atual, PoligonoRegular):
+            figura_atual.x2 = event.x
+            figura_atual.y2 = event.y
+
+    def soltar(self, event, figura_atual):
+        pass
+
+
 class EstadoSelecao(EstadoDesenho):
     def __init__(self, controller):
         self.controller = controller
@@ -88,7 +137,7 @@ class EstadoSelecao(EstadoDesenho):
     def pressionar(self, event, cor_borda, cor_preenchimento):
         figura = self.controller.model.buscar_figura_por_posicao(event.x, event.y)
         shift_pressionado = (event.state & 0x0001) != 0
-        
+
         if figura:
             if shift_pressionado:
                 if figura in self.controller.figuras_selecionadas:
@@ -101,7 +150,7 @@ class EstadoSelecao(EstadoDesenho):
         else:
             if not shift_pressionado:
                 self.controller.figuras_selecionadas.clear()
-        
+
         self.ultimo_x = event.x
         self.ultimo_y = event.y
         return None 
@@ -109,10 +158,10 @@ class EstadoSelecao(EstadoDesenho):
     def arrastar(self, event, figura_atual):
         dx = event.x - self.ultimo_x
         dy = event.y - self.ultimo_y
-        
+
         for figura in self.controller.figuras_selecionadas:
             figura.mover(dx, dy)
-            
+
         self.ultimo_x = event.x
         self.ultimo_y = event.y
 
