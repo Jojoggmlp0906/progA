521fe6ccf18bf21649231a0c9dafe807671dac6a
Jojoggmlp0906 <joadson.mauricio@dcomp.ufs.br>
2026-07-20 02:04:18 -0300

    Implement rectangular selection box in drawing controller
    Added rectangular selection box feature and updated event handling for figure manipulation.

--- projeto_completo/main.py
+++ projeto_completo/main.py
@@ -22,6 +22,7 @@ class DesenhoController:
         self.figura_atual = None       
         self.figuras_selecionadas = set() 
         self.copia_buffer = []            
+        self.caixa_selecao_retangular = None # Atributo para desenhar o retângulo de seleção em área
         self._conectar_eventos()
 
     def _obter_estado_atual(self):
@@ -44,39 +45,46 @@ class DesenhoController:
 
         self.view.bind("<Delete>", self._deletar_figuras)
         self.view.bind("<BackSpace>", self._deletar_figuras)
-        self.view.bind("<Shift-Right>", self._trazer_para_frente)
-        self.view.bind("<Shift-Left>", self._enviar_para_tras)
-        self.view.bind("<Shift-Up>", self._passo_para_frente)
-        self.view.bind("<Shift-Down>", self._passo_para_tras)
+
+        # Mapeamento para Mover nas Camadas
+        self.view.bind("<Up>", self._passo_para_frente)
+        self.view.bind("<Down>", self._passo_para_tras)
+        self.view.bind("<Shift-Up>", self._trazer_para_frente)
+        self.view.bind("<Shift-Down>", self._enviar_para_tras)
 
     def _ao_pressionar(self, event):
         estado = self._obter_estado_atual()
         if estado:
             retorno = estado.pressionar(event, self.view.cor_borda, self.view.cor_preenchimento)
-            
             if retorno:
                 self.figura_atual = retorno
                 if retorno not in self.model.obter_figuras():
                     self.model.adicionar_figura(self.figura_atual)
-                self.figura_selecionada = self.figura_atual
                 self.figuras_selecionadas = {self.figura_atual}
-            else:
+            elif self.view.ferramenta_atual.get() != "selecao":
                 self.figura_atual = None
 
-            self.view.atualizar_tela(self.model.obter_figuras(), self.figuras_selecionadas)
+            self._atualizar_tela()
 
     def _ao_arrastar(self, event):
         estado = self._obter_estado_atual()
         if estado:
             estado.arrastar(event, self.figura_atual)
-            self.view.atualizar_tela(self.model.obter_figuras(), self.figuras_selecionadas)
+            self._atualizar_tela()
 
     def _ao_soltar(self, event):
         estado = self._obter_estado_atual()
         if estado:
             estado.soltar(event, self.figura_atual)
             self.figura_atual = None
-            self.view.atualizar_tela(self.model.obter_figuras(), self.figuras_selecionadas)
+            self._atualizar_tela()
+
+    def _atualizar_tela(self):
+        self.view.atualizar_tela(
+            self.model.obter_figuras(),
+            self.figuras_selecionadas,
+            self.caixa_selecao_retangular
+        )
 
     def _copiar_figuras(self, event=None):
         if self.figuras_selecionadas:
@@ -91,61 +99,48 @@ class DesenhoController:
                 novos_clones.add(novo_clone)
             self.figuras_selecionadas = novos_clones
             self.copia_buffer = list(novos_clones)
-            self.view.atualizar_tela(self.model.obter_figuras(), self.figuras_selecionadas)
+            self._atualizar_tela()
 
     def _deletar_figuras(self, event=None):
         if self.figuras_selecionadas:
             for figura in list(self.figuras_selecionadas):
                 self.model.remover_figura(figura)
             self.figuras_selecionadas.clear()
-            self.view.atualizar_tela(self.model.obter_figuras(), self.figuras_selecionadas)
+            self._atualizar_tela()
 
     def _finalizar_poligono(self, event=None):
         estado = self._obter_estado_atual()
         if estado and hasattr(estado, "finalizar"):
             if estado.finalizar():
                 self.figura_atual = None
-                self.view.atualizar_tela(self.model.obter_figuras(), self.figura_selecionada)
+                self._atualizar_tela()
 
-    def _obter_figuras_selecionadas_em_ordem(self):
-        figuras_ordenadas = list(filter(self.figuras_selecionadas.__contains__, self.model.obter_figuras()))
-        return figuras_ordenadas
-    
     def _trazer_para_frente(self, event=None):
-        figuras_ordenadas = self._obter_figuras_selecionadas_em_ordem()
-        if figuras_ordenadas:
-            self.model.trazer_para_frente(figuras_ordenadas)
-            self.view.atualizar_tela(self.model.obter_figuras(), self.figuras_selecionadas)
+        if self.figuras_selecionadas:
+            self.model.trazer_para_frente(self.figuras_selecionadas)
+            self._atualizar_tela()
 
     def _enviar_para_tras(self, event=None):
-        figuras_ordenadas = self._obter_figuras_selecionadas_em_ordem()
-        if figuras_ordenadas:
-            self.model.enviar_para_tras(figuras_ordenadas)
-            self.view.atualizar_tela(self.model.obter_figuras(), self.figuras_selecionadas)
+        if self.figuras_selecionadas:
+            self.model.enviar_para_tras(self.figuras_selecionadas)
+            self._atualizar_tela()
 
     def _passo_para_frente(self, event=None):
-        figuras_ordenadas = self._obter_figuras_selecionadas_em_ordem()
-        if figuras_ordenadas:
-            self.model.trazer_um_passo_para_frente(figuras_ordenadas)
-            self.view.atualizar_tela(self.model.obter_figuras(), self.figuras_selecionadas)
+        if self.figuras_selecionadas:
+            self.model.trazer_um_passo_para_frente(self.figuras_selecionadas)
+            self._atualizar_tela()
 
     def _passo_para_tras(self, event=None):
-        figuras_ordenadas = self._obter_figuras_selecionadas_em_ordem()
-        if figuras_ordenadas:
-            self.model.enviar_um_passo_para_tras(figuras_ordenadas)
-            self.view.atualizar_tela(self.model.obter_figuras(), self.figuras_selecionadas)
+        if self.figuras_selecionadas:
+            self.model.enviar_um_passo_para_tras(self.figuras_selecionadas)
+            self._atualizar_tela()
 
     def _agrupar_figuras(self, event=None):
-        figuras_selecionadas = self._obter_figuras_selecionadas_em_ordem()
-        if len(figuras_selecionadas) < 2:
-            return
-
-        forma_composta = self.model.criar_forma_composta(figuras_selecionadas)
-        if forma_composta:
+        if len(self.figuras_selecionadas) >= 2:
+            figuras_ordenadas = [f for f in self.model.obter_figuras() if f in self.figuras_selecionadas]
+            forma_composta = self.model.criar_forma_composta(figuras_ordenadas)
             self.figuras_selecionadas = {forma_composta}
-            self.figura_selecionada = forma_composta
-            self.view.atualizar_tela(self.model.obter_figuras(), self.figuras_selecionadas)
-            print("Figuras agrupadas!")
+            self._atualizar_tela()
 
     def _selecionar_cor_borda(self, cor):
         self.view.cor_borda = cor
@@ -154,9 +149,7 @@ class DesenhoController:
                 figura.aplicar_cor_borda(cor)
             elif hasattr(figura, "cor_borda"):
                 figura.cor_borda = cor
-        if self.figuras_selecionadas:
-            self.figura_selecionada = next(iter(self.figuras_selecionadas))
-        self.view.atualizar_tela(self.model.obter_figuras(), self.figuras_selecionadas)
+        self._atualizar_tela()
 
     def _selecionar_cor_preenchimento(self, cor):
         cor_final = cor if cor else ""
@@ -166,20 +159,11 @@ class DesenhoController:
                 figura.aplicar_cor_preenchimento(cor_final)
             elif hasattr(figura, "cor_preenchimento"):
                 figura.cor_preenchimento = cor_final
-        if self.figuras_selecionadas:
-            self.figura_selecionada = next(iter(self.figuras_selecionadas))
-        self.view.atualizar_tela(self.model.obter_figuras(), self.figuras_selecionadas)
-            
-    def executar(self, cor):
-        cor_final = cor if cor else ""
-        self.view.cor_preenchimento = cor_final
-        for figura in self.figuras_selecionadas:
-            if hasattr(figura, "cor_preenchimento"):
-                figura.cor_preenchimento = cor_final
-        self.view.atualizar_tela(self.model.obter_figuras(), self.figuras_selecionadas)
+        self._atualizar_tela()
 
     def executar(self):
         self.view.mainloop()
+
 
 if __name__ == "__main__":
     app = DesenhoController()
